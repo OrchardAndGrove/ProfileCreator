ec032eed7e6caf2102cf13200b19ce3bfb5d91da
Erik Berglund <erikberglund@Eriks-MacBook-Pro.local>
2017-07-07 23:14:59 +0200

    Register user defaults

--- ProfileCreator/ProfileCreator/AppDelegate.swift
+++ ProfileCreator/ProfileCreator/AppDelegate.swift
@@ -22,9 +22,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillFinishLaunching(_ notification: Notification) {
         
         // ---------------------------------------------------------------------
+        //  Register user defaults
+        // ---------------------------------------------------------------------
+        registerDefaults()
+        
+        // ---------------------------------------------------------------------
         //  Show main window
         // ---------------------------------------------------------------------
         mainWindowController.window?.makeKeyAndOrderFront(self)
+    }
+    
+    func registerDefaults() {
+        
+        // ---------------------------------------------------------------------
+        //  Get URL to application default settings
+        // ---------------------------------------------------------------------
+        guard let defaultSettingsURL = Bundle.main.url(forResource: "Defaults", withExtension: "plist") else {
+            // TODO: Proper logging
+            print("No Defaults file found!")
+            return
+        }
+        
+        // ---------------------------------------------------------------------
+        //  Register default settings with UserDefaults
+        // ---------------------------------------------------------------------
+        if  let defaultSettingsDict = NSDictionary(contentsOf: defaultSettingsURL),
+            let defaultSettings = defaultSettingsDict as? [String : Any] {
+            UserDefaults.standard.register(defaults: defaultSettings)
+        }
     }
 }
 
